14736859f55fa933486e8b6f9d1b44b40e9caa4b
blanc <blanc@BLANCA.local>
2024-05-28 09:17:49 +0200

    Clase Mano modificada
    Modificado el método valorMano para que los ases puedan valer tanto 1
    como 11, añadido el método finDeJuego y toString

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -10,11 +10,38 @@ public class Mano extends Mazo {
 		
 	}
 
-	public int valorMano() {
-		int valor=0;
-		for(Carta c : this.cartas) {
-			valor = valor + c.getValor();
-		}
+	public int valorMano()  {
+			int valor=0;
+			int numAses=0;
+			for(Carta c : this.cartas) {
+				if (c.getNumero()==1) {
+					numAses++;
+				}
+				valor = valor + c.getValor();
+			}
+			//comprobamos si me he  pasado de 21 
+			while (numAses>0 && valor>21) {
+				valor=valor-10;
+				numAses--;
+			}
+			if (valor>21) {
+				throw new Masde21Exception();
+			}
 		return valor;
 	}
+
+	@Override
+	public String toString() {
+		return "Mano [cartas=" + cartas + "]Valor de la mano:"+valorMano();
+	}
+	
+	
+	 /*
+	  
+	  
+	
+		}
+		return false;
+	}*/
+	
 }
